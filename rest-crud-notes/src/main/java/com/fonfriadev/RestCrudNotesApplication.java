29019c774fda126d4db28e846bfc0830337eda84
danie <leinadfonfria@gmail.com>
2021-08-09 20:33:20 +0200

    rest crud notes init

---
+++ rest-crud-notes/src/main/java/com/fonfriadev/RestCrudNotesApplication.java
@@ -0,0 +1,13 @@
+package com.fonfriadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCrudNotesApplication.class, args);
+	}
+
+}
